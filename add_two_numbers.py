c734ff11d3a8c1fb36980f3eda9d943d5474c75f
NgariMutugi <NgariMutugi@github.com>
2018-06-28 20:02:46 +0300

    change

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,10 +1,10 @@
 # This program adds two numbers
 
-num_1 = 1.5
-num_2 = 6.3
+NUM1 = 1.5
+NUM2 = 6.3
 
 # Add two numbers
-add = float(num_1) + float(num_2)
+add = float(NUM1) + float(NUM2)
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num_1, num_2, add))
+print('The sum of {0} and {1} is {2}'.format(NUM1, NUM2, add))
